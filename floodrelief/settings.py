e0cde45145234d586d343e2e5a5bae3200f5a8d6
Aswin G <aswinganesh666@gmail.com>
2018-08-16 12:53:17 +0530

    Temp

--- floodrelief/settings.py
+++ floodrelief/settings.py
@@ -111,8 +111,17 @@ WSGI_APPLICATION = 'floodrelief.wsgi.application'
 #     # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
 #     'default': env.db()
 # }
-DATABASES = {}
-DATABASES['default'] = dj_database_url.parse(env('B_DATABASE_URL'), conn_max_age=600)
+#DATABASES['default'] = dj_database_url.parse(env('B_DATABASE_URL'), conn_max_age=600)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': '',
+    }
+}
 
 CACHES = {
     "default": {
